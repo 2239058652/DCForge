2b9b31a933440b5be31732182f2eca929159e756
xiaoding <2239058652@qq.com>
2026-04-29 11:12:00 +0800

    删除多余的东西

--- src/components/FileUpload/ImageViewer.tsx
+++
@@ -1,9 +0,0 @@
-import { Image } from 'antd'
-
-interface Props {
-    url: string
-}
-
-export default function ImageViewer({ url }: Props) {
-    return <Image src={url} alt="preview" className="max-w-full h-auto overflow-auto" />
-}
